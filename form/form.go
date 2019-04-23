bd221e0ba373f0b8faed44c3418335da52f3367c
liyang <1362605516@qq.com>
2019-04-23 10:04:25 +0800

    web 服务器

--- form/form.go
+++ form/form.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()       //解析url传递的参数，对于POST则解析响应包的主体（request body）
 	//注意:如果没有调用ParseForm方法，下面无法获取表单的数据
 	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
+
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
@@ -25,18 +31,71 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //获取请求的方法
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.gtpl")
-		log.Println(t.Execute(w, nil))
+		cur_time := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(cur_time, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, err := template.ParseFiles("login.html")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(t.Execute(w, token))
 	} else {
 		//请求的是登录数据，那么执行登录的逻辑判断
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
+		r.ParseForm()
+
+		token := r.Form.Get("token")
+		if token == "" {
+			fmt.Fprintln(w,"error!")
+			return
+		} else {
+			//todo 验证token的合法性
+		}
+		fmt.Println("username:", r.Form.Get("username"))
+		fmt.Println("password:", r.Form.Get("password"))
+		template.HTMLEscape(w, []byte(r.Form.Get("username")));
+	}
+}
+func upload(w http.ResponseWriter, r *http.Request)  {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		cur_time := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(cur_time, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, err := template.ParseFiles("upload.html")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(t.Execute(w, token))
+	} else if r.Method == "POST" {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./" + handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
 	}
 }
 
 func main() {
 	http.HandleFunc("/", sayhelloName)       //设置访问的路由
 	http.HandleFunc("/login", login)         //设置访问的路由
+	http.HandleFunc("/upload", upload)
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
